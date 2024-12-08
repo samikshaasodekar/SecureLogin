2c5e1a661ec1b7388ac661114b6a7f931836ba3a
Samiksha <112839465+samikshaasodekar@users.noreply.github.com>
2024-12-08 07:53:30 +0530

    Update SplashScreenView.java

--- src/main/java/com/example/SecureLogin/SplashScreenView.java
+++ src/main/java/com/example/SecureLogin/SplashScreenView.java
@@ -14,20 +14,17 @@ public class SplashScreenView extends JFrame {
         setLocationRelativeTo(null);
         setLayout(null); 
 
-        // Load and scale the background image to fit the frame
         ImageIcon originalImageIcon = new ImageIcon(getClass().getClassLoader().getResource("images/sl.jpg"));
         Image scaledImage = originalImageIcon.getImage().getScaledInstance(400, 400, Image.SCALE_SMOOTH);
         JLabel backgroundLabel = new JLabel(new ImageIcon(scaledImage));
         backgroundLabel.setBounds(0, 0, 400, 400);
         add(backgroundLabel);
 
-        // Progress Bar
         progressBar = new JProgressBar();
         progressBar.setBounds(50, 300, 300, 20);
         progressBar.setForeground(new Color(0, 128, 128));
         backgroundLabel.add(progressBar);
 
-        // Set the backgroundLabel layout to null to ensure it holds absolute positions
         backgroundLabel.setLayout(null);
     }
 
